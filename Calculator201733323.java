9ec02d7785e0b2eab0b84e916995e2733213b36f
Goodwill <201733323@ump.ac.za>
2020-07-29 15:45:41 +0200

    Goodwill First Commit

---
+++ Calculator201733323.java
@@ -0,0 +1,16 @@
+public class Calculator201733323{
+
+public static void main(String[] args) {
+ double x = 80,
+        y = 70;
+        double add = x+y;
+        double subtract = x-y;
+        double divide = x/y;
+        double multiply = x*y;
+        System.out.println(y+" + "+x+" = "+add);
+        System.out.println(y+" - "+x+" = "+subtract);
+        System.out.println(y+" / "+x+" = "+divide);
+        System.out.println(y+" x "+x+" = "+multiply);
+ }
+}
+
